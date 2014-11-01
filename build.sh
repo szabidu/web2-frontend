daf1a7da6602fd0d3d90dbb3f314d85584ba2c0e
Elek, Márton <level2@anix.net>
2014-11-01 19:34:45 +0100

    chore(buid): make the project stanalone buildable

---
+++ build.sh
@@ -0,0 +1,10 @@
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p $DIR/build
+npm install
+bower update
+gulp build
+cd $DIR/dist
+rm $DIR/build/frontend.zip
+zip -r $DIR/build/frontend.zip *
+cd $DIR
