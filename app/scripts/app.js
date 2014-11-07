3606945f4296d6fb6491b2313ce4cb5d90706fff
Elek, Márton <level2@anix.net>
2014-11-07 21:43:51 +0100

    feat(analytics): more detailed google analytics stat

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,16 @@
 'use strict';
-var tilos = angular.module('tilosApp', ['ngRoute', 'ngSanitize', 'configuration', 'ui.bootstrap', 'textAngular', 'ngResource', 'ui.router', 'LocalStorageModule','vcRecaptcha']);
+var tilos = angular.module('tilosApp',
+  ['ngRoute',
+    'ngSanitize',
+    'configuration',
+    'ui.bootstrap',
+    'textAngular',
+    'ngResource',
+    'ui.router',
+    'LocalStorageModule',
+    'vcRecaptcha',
+    'angulartics',
+    'angulartics.google.analytics']);
 
 tilos.config(function ($locationProvider) {
     $locationProvider.html5Mode(true);
