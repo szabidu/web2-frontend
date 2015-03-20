e51abc5d5878886a366e162bfcc3f9c3b2122a2f
Elek, Márton <level2@anix.net>
2015-03-20 21:23:00 +0100

    fix user register form

--- app/scripts/controllers/Register.js
+++ app/scripts/controllers/Register.js
@@ -10,7 +10,7 @@ angular.module('tilosApp').config(function ($stateProvider) {
 
 });
 
-angular.module('tilosApp').controller('RegisterCtrl', function ($scope, $http, API_SERVER_ENDPOINT, vcRecaptchaService, $rootScope, localStorageService, $location) {
+angular.module('tilosApp').controller('RegisterCtrl', function ($scope, $http, API_SERVER_ENDPOINT, vcRecaptchaService, $rootScope, localStorageService, $location, satellizer) {
     if ($scope.user) {
         $location.path('/me');
     }
@@ -20,8 +20,8 @@ angular.module('tilosApp').controller('RegisterCtrl', function ($scope, $http, A
         $scope.form.captchaChallenge = vcRecaptchaService.data().challenge;
         $scope.form.captchaResponse = vcRecaptchaService.data().response;
         $http.post(API_SERVER_ENDPOINT + '/api/v1/auth/register', $scope.form).success(function (data) {
+            satellizer.setToken(data)
             localStorageService.set('jwt', data);
-
             $http.get(API_SERVER_ENDPOINT + '/api/v1/user/me').success(function (data) {
                 $rootScope.user = data;
                 $location.path('/');
