658512919c95843392fdd2c93a9b2069ca73b14b
Elek, Márton <level2@anix.net>
2015-07-17 21:25:40 +0200

    fix: remove old counter

--- app/scripts/controllers/Main.js
+++ app/scripts/controllers/Main.js
@@ -31,22 +31,4 @@ angular.module('tilosApp').controller('MainCtrl', function ($scope, FeedService,
         $scope.lead = data;
     });
 
-    var myTimeout;
-    $scope.counter = '';
-    var deadline = new Date(2014, 6, 23, 11, 59, 40).getTime();
-    $scope.onTimeout = function () {
-        var diff = new Date().getTime() - deadline;
-        var days = Math.floor(diff / (60 * 60 * 24 * 1000));
-        diff = diff - days * 60 * 60 * 24 * 1000;
-        var hours = Math.floor(diff / (60 * 60 * 1000));
-        diff = diff - hours * 60 * 60 * 1000;
-        var minutes = Math.floor(diff / (60 * 1000));
-        diff = diff - minutes * 60 * 1000;
-        var seconds = Math.floor(diff / 1000);
-        myTimeout = $timeout($scope.onTimeout, 1000);
-        $scope.counter = '' + days + ' nap, ' + hours + ' óra, ' + minutes + ' perc és ' + seconds + ' másodperc';
-    };
-
-    $scope.onTimeout();
-
 });
