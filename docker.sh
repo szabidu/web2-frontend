05b5124fd7f5c85831346d4dc28c91b15da3d405
Elek, Márton <level2@anix.net>
2015-10-18 17:14:05 +0200

    new: enable to push the internal repository

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 docker build -t tilosradio/frontend .
-docker tag -f tilosradio/frontend tilos:5555/tilosradio/frontend
+docker tag -f tilosradio/frontend tilos.hu:5555/tilosradio/frontend
 sudo docker push tilos.hu:5555/tilosradio/frontend
 
