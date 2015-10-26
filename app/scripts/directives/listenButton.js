6be23bec6104f7c5984cd05c660360bfb96cec29
Elek, Márton <level2@anix.net>
2015-10-26 22:39:25 +0100

    new: separated download link for forced file download

--- app/scripts/directives/listenButton.js
+++ app/scripts/directives/listenButton.js
@@ -9,6 +9,7 @@ angular.module('tilosApp').directive('listenButton', function ($compile) {
         templateUrl: 'partials/listenButton.html',
         controller: function($scope) {
             $scope.mp3Link = $scope.link.replace('.m3u','.mp3');
+            $scope.downloadLink = $scope.mp3Link.replace("/mp3/","/download/");
         }
     }
 
