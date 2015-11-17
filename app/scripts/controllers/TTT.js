ac32f199cab76d75f1fac165a000d73f5c6f73e8
Laszlo Heves <laszlo.heves@gawker.com>
2015-11-17 11:45:22 +0100

    new: ttt businesses

--- app/scripts/controllers/TTT.js
+++ app/scripts/controllers/TTT.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('tilosApp').config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.when('/ttt', '/ttt/elfogad');
-    $urlRouterProvider.when('/ttt/', '/ttt/elfogad');
+    var businessUrlSlug = 'elfogad';
+
+    $urlRouterProvider.when('/ttt', '/ttt/' + businessUrlSlug);
+    $urlRouterProvider.when('/ttt/', '/ttt/' + businessUrlSlug);
 
     $stateProvider.state('ttt', {
         abstract: true,
@@ -12,23 +14,25 @@ angular.module('tilosApp').config(function ($stateProvider, $urlRouterProvider) 
 
     $stateProvider.state('ttt.sub', {
         url: '/:id',
-        templateUrl: 'partials/ttt-sub.html',
+        templateUrl: function (stateParams) {
+            return (stateParams.id === businessUrlSlug) ? 'partials/ttt-business.html' : 'partials/ttt-sub.html';
+        },
         controller: 'TTTCtrl'
     });
 
-    $stateProvider.state('ttt.default', {
-        url: '',
-        templateUrl: 'partials/ttt-sub.html',
-        controller: 'TTTCtrl'
-    });
-})
+});
 
 angular.module('tilosApp').controller('TTTCtrl', function ($scope, API_SERVER_ENDPOINT, $stateParams, $http) {
-    var id = $stateParams.id
-    if (id == undefined || id === '') {
-        id = 'havi';
-    }
-    $http.get(API_SERVER_ENDPOINT + '/api/v1/text/page/ttt-' + id).success(function (data) {
-        $scope.page = data;
-    });
+    var id = $stateParams.id;
+
+    if (id === 'elfogad') {
+        $http.get(API_SERVER_ENDPOINT + '/api/v1/ttt/business').success(function (data) {
+		  $scope.businesses = data;
+  	    });
+  } else {
+        $http.get(API_SERVER_ENDPOINT + '/api/v1/text/page/ttt-' + id).success(function (data) {
+            $scope.page = data;
+        });
+  }
+
 });
