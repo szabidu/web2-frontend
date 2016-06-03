f64a81b0a2052af7b7236777d652e4d5b7d07c12
Elek, Márton <elek@users.noreply.github.com>
2016-06-03 20:41:03 +0200

    fix: date issue for firefox

--- client/maraton/Maraton.js
+++ client/maraton/Maraton.js
@@ -19,61 +19,68 @@ angularModule.factory('pouchService', function ($rootScope) {
     });
     return db;
 })
-    .run(function (pouchService) {
+.run(function (pouchService) {
 
-        
+});
+
+
+angularModule.controller('DayCtrl', function ($scope, pouchService, $rootScope) {
+    var load = function () {
+        var base = new Date("2016-06-03T06:00:00.000Z").getTime()
+        var date = Math.max(base, new Date().getTime());
+        var offset = Math.floor((date - base) / (60 * 60 * 1000 * 24)) + 1;
+        var id = "day" + offset;
+        pouchService.get(id, {attachment: true}).then(function (result) {
+            $scope.$apply(function () {
+                $scope.day = result;
+            });
+            $scope.selectIcon = function (type) {
+                if (type == 'radio') {
+                    return 'ion-mic-c';
+                } else if (type == 'tombola') {
+                    return 'ion-speakerphone';
+                } else if (type == 'DJ') {
+                    return 'ion-disc';
+                } else if (type == 'bogracs') {
+                    return 'ion-fork';
+                } else {
+                    return '';
+                }
+            };
+            $scope.timeComparator = function (item) {
+                if (item.time && item.time.length > 4) {
+                    var hour = parseInt(item.time.substring(0, 2))
+                    var min = parseInt(item.time.substring(3, 5))
+                    if (hour < 12) hour += 24;
+                    return hour * 60 + min;
+                }
+                return -1;
+            };
+
+            angular.forEach(result.events, function (event) {
+                if (event.image) {
+                    pouchService.getAttachment(id, event.image).then(function (data) {
+
+                        $scope.$apply(function () {
+                            event.imageData = URL.createObjectURL(data);
+                        });
+                        //event.imageData = 'http://localhost:5984/maraton16/day1/bazsinka.jpg';
+
+
+                    }).catch(function (err) {
+                        console.log(err);
+                    });
+                }
+
+            });
+
+        })
+    };
+    load();
+    $rootScope.$on("synced", function () {
+        load();
     });
 
-
-angularModule.controller('DayCtrl', function ($scope, pouchService) {
-    var base = new Date("2016-06-03 08:00").getTime()
-    var date = Math.max(base, new Date().getTime());
-    var offset = Math.floor((date - base) / (60 * 60 * 1000 * 24)) + 1;
-    var id = "day" + offset;
-    pouchService.get(id, {attachment: true}).then(function (result) {
-        $scope.$apply(function () {
-            $scope.day = result;
-        });
-        $scope.selectIcon = function (type) {
-            if (type == 'radio') {
-                return 'ion-mic-c';
-            } else if (type == 'tombola') {
-                return 'ion-speakerphone';
-            } else if (type == 'DJ') {
-                return 'ion-disc';
-            } else if (type == 'bogracs') {
-                return 'ion-fork';
-            } else {
-                return '';
-            }
-        };
-        $scope.timeComparator = function (item) {
-            if (item.time && item.time.length > 4) {
-                var hour = parseInt(item.time.substring(0, 2))
-                var min = parseInt(item.time.substring(3, 5))
-                if (hour < 12) hour += 24;
-                return hour * 60 + min;
-            }
-            return -1;
-        };
-
-        angular.forEach(result.events, function (event) {
-            if (event.image) {
-                pouchService.getAttachment(id, event.image).then(function (data) {
-
-                    $scope.$apply(function () {
-                        event.imageData = URL.createObjectURL(data);
-                    });
-                    //event.imageData = 'http://localhost:5984/maraton16/day1/bazsinka.jpg';
-
-
-                }).catch(function (err) {
-                    console.log(err);
-                });
-            }
-
-        });
-    })
 });
 
 module.exports = angularModule;
