fdff5a87082a4c3db2be70e5fe93506919693871
rudywaltz <david.barkoczi@gmail.com>
2018-10-18 09:31:52 +0200

    truncate too long description #231

--- client/app.js
+++ client/app.js
@@ -46,6 +46,24 @@ tilos.config(['growlProvider', function (growlProvider) {
     growlProvider.globalPosition('top-left');
 }]);
 
+tilos.filter('truncate', function () {
+    return function (value, wordwise, max, tail) {
+        if (!value) return '';
+        max = parseInt(max, 10);
+        if (!max) return value;
+        if (value.length <= max) return value;
+
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace != -1) {
+                value = value.substr(0, lastspace);
+            }
+        }
+        return value + (tail || ' …');
+    };
+});
+
 
 tilos.config(function ($locationProvider, $httpProvider) {
     $locationProvider.html5Mode(true);
