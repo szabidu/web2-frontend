16a49da6b8b6cdf576633c8a72cfb5f2619b46dc
szabidu <szabi@tilos.hu>
2019-06-13 06:36:53 +0200

    pénz

--- client/main/Maraton.js
+++ client/main/Maraton.js
@@ -86,7 +86,7 @@ angularModule.controller('MaratonCtrl', function ($scope) {
         major: 2,
         min: 0,
         max: 15000,
-        value: 7651
+        value: 9703
     }
     var target = document.getElementById("donation--indicator--wrapper");
     var indicator = new Indicator(params, target);  
